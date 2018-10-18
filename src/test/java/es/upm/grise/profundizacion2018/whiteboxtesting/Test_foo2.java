ba39a2a7f9c51e8b088fc6bb4a8918595711dc0a
Oscar Dieste <odieste@fi.upm.es>
2018-10-18 22:19:19 +0200

    Ejercicios

---
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
@@ -0,0 +1,14 @@
+package es.upm.grise.profundizacion2018.whiteboxtesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_foo2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_maxAbsolute.java
@@ -0,0 +1,14 @@
+package es.upm.grise.profundizacion2018.whiteboxtesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_maxAbsolute {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
