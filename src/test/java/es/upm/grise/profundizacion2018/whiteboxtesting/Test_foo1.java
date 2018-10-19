b53108fa613f0bbe8e9df9b3aff21a900b83deae
PuppedToy <mapastzar@hotmail.com>
2018-10-19 16:19:46 +0200

    foo1 tests & first foo2 test

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo1.java
@@ -6,9 +6,24 @@ import org.junit.Test;
 
 public class Test_foo1 {
 
+	private Exercises ex = new Exercises();;
+		
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void x4doesnotadd10() {
+		float x = 4;
+		float y = 8;
+		float actual = ex.foo1(x, y);
+		float expected = 2;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual, 0.01);
+	}
+	
+	@Test
+	public void x6doesadd10() {
+		float x = 6;
+		float y = 16;
+		float actual = ex.foo1(x, y);
+		float expected = 1;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual, 0.01);
 	}
 
 }
--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
@@ -6,9 +6,15 @@ import org.junit.Test;
 
 public class Test_foo2 {
 
+	Exercises ex = new Exercises();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void aIsGreaterThanBAndCosAisLessThan0() {
+		float a = 5; // cos = 0.28
+		float b = 2; // cos = -0.41
+		float actual = ex.foo2(a, b);
+		float expected = 2;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual, 0.01);
 	}
 
 }
