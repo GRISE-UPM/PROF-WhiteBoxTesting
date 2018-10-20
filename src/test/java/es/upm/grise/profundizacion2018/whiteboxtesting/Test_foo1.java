32aa08e766fa3c1689ea492bfeb295bfda5fc30c
Pablo Lopez <pablolopan@gmail.com>
2018-10-20 15:37:50 +0200

    Gitignore y tests para foo1

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo1.java
@@ -1,14 +1,26 @@
 package es.upm.grise.profundizacion2018.whiteboxtesting;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.Before;
 
 public class Test_foo1 {
+	
+	private Exercises ex;
+	
+	@Before
+	public void loadClass() {
+		ex = new Exercises();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void xGreaterThanFive() {
+		assertEquals(ex.foo1(10, 100), 5, 0.001);
+	}
+	
+	@Test
+	public void xLessThanFive() {
+		assertEquals(ex.foo1(2, 24), 12, 0.001);
 	}
 
 }
