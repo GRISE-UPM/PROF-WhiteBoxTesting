2f222f7ba0e67862c69393465d218f9d42b5c7bd
Pablo Lopez <pablolopan@gmail.com>
2018-10-21 13:49:17 +0200

    Added 100% coverage tests for foo2

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
@@ -2,13 +2,39 @@ package es.upm.grise.profundizacion2018.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_foo2 {
+	
+	private Exercises ex;
+	
+	@Before
+	public void loadClass() {
+		ex = new Exercises();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void fortyFiveAndZero() {
+		assertEquals(ex.foo2((float) Math.PI/4, 0f), 2, 0.001);
 	}
+		
+	@Test
+	public void minusPiAndHundredThirtyFive() {
+		float hundredThirtyFive = (float) (Math.PI/4f)*3f;
+		assertEquals(ex.foo2((float) -Math.PI, hundredThirtyFive), 2, 0.001);
+	}
+	
+	@Test
+	public void ninetyAndZero() {
+		assertEquals(ex.foo2((float) Math.PI/2, 0f), Math.PI/4, 0.001);
+	}
+	
+	@Test
+	public void minusFortyFiveAndFortyFive() {
+		assertEquals(ex.foo2((float) Math.PI/4, (float) -Math.PI/4), 2, 0.001);
+	}
+	
+
 
 }
