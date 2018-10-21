4db6ad8e88631b1f3083c72e4016c65e9c955997
Pablo Lopez <pablolopan@gmail.com>
2018-10-21 13:55:39 +0200

    Added 100% cobertura tests for max_absolute

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_maxAbsolute.java
@@ -2,13 +2,34 @@ package es.upm.grise.profundizacion2018.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_maxAbsolute {
-
+	
+	private Exercises ex;
+	
+	@Before
+	public void loadClass() {
+		ex = new Exercises();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void arrayTooLong() {
+		int[] nums = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+		assertEquals(ex.max_absolute(nums), -1, 0.001);
+	}
+	
+	@Test
+	public void normalMax() {
+		int[] nums = {1, 2, 3, 2};
+		assertEquals(ex.max_absolute(nums), 3, 0.001);
+	}
+	
+	@Test
+	public void negativeAbsoluteMax() {
+		int[] nums = {1, 2, -3, 2};
+		assertEquals(ex.max_absolute(nums), 3, 0.001);
 	}
 
 }
