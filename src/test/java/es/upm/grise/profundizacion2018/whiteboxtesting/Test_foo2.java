aa94d51ebce0c572f96362f6dea8966b99faacb2
PuppedToy <mapastzar@hotmail.com>
2018-10-22 16:19:07 +0200

    Finished foo2 tests with max coverage

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
@@ -8,13 +8,32 @@ public class Test_foo2 {
 
 	Exercises ex = new Exercises();
 	
+	// Careful, a & b swaps in this test
 	@Test
-	public void aIsGreaterThanBAndCosAisLessThan0() {
+	public void aIsGreaterThanB_cosAisLessThan0_cosBisMoreThan0() {
 		float a = 5; // cos = 0.28
 		float b = 2; // cos = -0.41
 		float actual = ex.foo2(a, b);
 		float expected = 2;
 		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual, 0.01);
 	}
-
+	
+	@Test 
+	public void aIsLowerThanB_cosAIsMoreThan0_cosBisMoreThan0() {
+		float a, b;
+		a = b = 5; // cos = 0.28
+		float actual = ex.foo2(a, b);
+		float expected = 2;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual, 0.01);
+	}
+	
+	@Test
+	public void aIsLowerThanB_cosAIsMoreThan0_cosBisLessThan0() {
+		float a = 5; // cos = 0.28
+		float b = 9; // cos = -0.91
+		float actual = ex.foo2(a, b);
+		float expected = 7;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual, 0.01);
+	}
+	
 }
