2cd4451b450e83039ead509637fa40a98e9cd2a4
Jorge Forcada <jorge_forcada@hotmail.com>
2018-10-23 00:56:12 +0200

    Add maxAbsolute unit tests

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_maxAbsolute.java
@@ -4,11 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Complejidad Ciclomática de 4:
+ * para los caminos bastarían con 4 tests, 
+ * para evaluar todas las condiciones hacen falta 4 tests
+ */
 public class Test_maxAbsolute {
 
+	private Exercises exercises = new Exercises();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void max_absolute_LengthEquals0() {
+		assertEquals((int)exercises.max_absolute(new int[0]), 0);
+	}
+	
+	@Test
+	public void max_absolute_LengthGreaterThan5() {
+		assertEquals((int)exercises.max_absolute(new int[6]), -1);
+	}
+	
+	@Test
+	public void max_absolute_LengthLessOrEqualsThan5AndSecondElementIsMax() {
+		assertEquals((int)exercises.max_absolute(new int[] {1, 4, 3, 2}), 4);
+	}
+	
+	@Test
+	public void max_absolute_LengthLessOrEqualsThan5AndNegativeElementIsMax() {
+		assertEquals((int)exercises.max_absolute(new int[] {-5, 4, 3, 2}), 5);
 	}
 
 }
