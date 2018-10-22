c2583dd47cf9a6ad55ab08cb579929e793bcf711
PuppedToy <mapastzar@hotmail.com>
2018-10-22 17:59:06 +0200

    Tests for ConvertInt

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_convertInt.java
@@ -4,11 +4,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import es.upm.grise.profundizacion2018.whiteboxtesting.Exercises.ConvertInt;
+
 public class Test_convertInt {
 
+	private Exercises ex = new Exercises();
+	private ConvertInt ci = (ex).new ConvertInt();
+	
+	@Test(expected = Exception.class)
+	public void more_than_5_elements() throws Exception {
+		char[] str = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+		ci.convert(str);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void starting_with_hyphen() throws Exception {
+		char[] str = {'-', '\5', '\1', '\0', '\0'};
+		int actual = ci.convert(str);
+		int expected = -5100;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual);
+	}
+	
+	@Test
+	public void starting_without_hyphen() throws Exception {
+		char[] str = {'\5', '\1', '\0', '\0'};
+		int actual = ci.convert(str);
+		int expected = 5100;
+		assertEquals("Result should be " + expected + ", but is " + actual, expected, actual);
+	}
+	
+	@Test(expected = Exception.class)
+	public void bigger_than_32767() throws Exception {
+		char[] str = {'\5', '\1', '\0', '\0', '\1'};
+		ci.convert(str);
+	}
+	
+	@Test(expected = Exception.class)
+	public void smaller_than_minus32767() throws Exception {
+		char[] str = {'-', '\5', '\1', '\0', '\0', '\1'};
+		ci.convert(str);
 	}
 
 }
