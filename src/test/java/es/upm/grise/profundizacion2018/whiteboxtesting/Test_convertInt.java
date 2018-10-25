5b6a85fced806d080559e9e803c81d43f0f5e33f
mirigaray <meryiri95@gmail.com>
2018-10-25 14:17:58 +0200

    Test_convertInt modified

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_convertInt.java
@@ -7,17 +7,91 @@ import org.junit.Test;
 
 public class Test_convertInt {
         private  ConvertInt exToTest = new Exercises().new ConvertInt();
+        /**
+         * Test pata ver si salta excepcion ante un array vacio
+         */
 	@Test
-	public void test() {
+	public void test_1() {
 		//fail("Not yet implemented");
                 try{
                     exToTest.convert(new char[] {});        
                     fail("No ha saltado la Ex."); 
                 }
                 catch(Exception ex){
-                    //assertTrue(true);
+                    //Ha saltado ls excepcion como debía hacer
                 }
                             
 	}
+        /**
+         * Test para ver si salta una excepcion ante un array mayor que 6
+         */
+        @Test
+	public void test_2() {
+		//fail("Not yet implemented");
+                try{
+                    exToTest.convert(new char[] {'a','a','a','a','a','a','a'});        
+                    fail("No ha saltado la Ex."); 
+                }
+                catch(Exception ex){
+                    //Ha saltado ls excepcion como debía hacer
+                }
+                            
+	}
+        /**
+         * Test para ver si salta una exceppcion ante caracteres no deseados
+         */
+        @Test
+	public void test_3() {
+		//fail("Not yet implemented");
+                try{
+                    exToTest.convert(new char[] {'a','a','a','a','a'});        
+                    fail("No ha saltado la Ex."); 
+                }
+                catch(Exception ex){
+                    //Ha saltado ls excepcion como debía hacer
+                }
+                            
+	}
+        /**
+         * Test para ver si convierte adecuadamente ASCI
+         */
+        @Test
+	public void test_4() {
+            try{
+                assertEquals((int)exToTest.convert(new char[] { '3','4'}), 562);
+            }
+            catch(Exception ex){
+                fail("No deberia haber saltado una Ex.");
+            }
+		
+	}
+	/**
+         * Test para ver si salta una excepcion ante numeros mayores que 32767
+         */
+	@Test 
+	public void test_5() {
+            try{
+                exToTest.convert(new char[] {'2', '3','4','5','6'});
+                fail();
+            }
+            catch(Exception ex){
+                fail("No deberia haber saltado una Ex.");
+            }		
+	}
+        /**
+         * Test para ver si salta una excepcion ante numeros menores que -32767
+         */
+        @Test 
+	public void test_6() {
+            try{
+                exToTest.convert(new char[] {'-','2', '3','4','5','6'});
+                fail("No ha saltado la Ex.");
+            }
+            catch(Exception ex){
+                //Ha saltado ls excepcion como debía hacer
+            }		
+	}
+	
+        
 
 }
