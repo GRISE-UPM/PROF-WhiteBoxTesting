32798638a770718d66e26b987776b6409a4f667a
Víctor Vaquerizo <v.vaquerizo@alumnos.upm.es>
2018-10-27 01:41:10 +0200

    Implemented convertInt tests

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_convertInt.java
@@ -16,12 +16,34 @@ public class Test_convertInt {
 		convertint = exercises.new ConvertInt();
 	}
 
-	/*@Test
-	public void test1() {
-		int[] i = {1,2,3,4,5,6};
-		assertEquals(-1,this.convertint.convert(),0);
-	}*/
+	@Test(expected = Exception.class)
+	public void test1() throws Exception {
+		char[] string = {'1','2','3','4','5','6','7'};
+		assertEquals(Exception.class,this.convertint.convert(string));
+	}
 	
-
+	@Test
+	public void test2() throws Exception {
+		char[] string = {'2','3','1'};
+		assertEquals(231,this.convertint.convert(string));
+	}
+	
+	@Test
+	public void test3() throws Exception {
+		char[] string = {'-','3'};
+		assertEquals(-3,this.convertint.convert(string));
+	}
+	
+	@Test(expected = Exception.class)
+	public void test4() throws Exception {
+		char[] string = {'-','9','9','9','9','9'};
+		assertEquals(Exception.class,this.convertint.convert(string));
+	}
+	
+	@Test(expected = Exception.class)
+	public void test5() throws Exception {
+		char[] string = {'9','9','9','9','9'};
+		assertEquals(Exception.class,this.convertint.convert(string));
+	}
 
 }
