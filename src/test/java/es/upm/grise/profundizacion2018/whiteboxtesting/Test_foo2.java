19305e87540c7a43e2f8ef6009d5eb6a261ac918
jlcastanon <jl.castanon@alumnos.upm.es>
2018-11-16 19:53:17 +0100

    whitebox txt

--- src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion2018/whiteboxtesting/Test_foo2.java
@@ -6,9 +6,33 @@ import org.junit.Test;
 
 public class Test_foo2 {
 
+
+	Exercises ex = new Exercises();
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void aMajorbCosaTrue(){
+		assertEquals(2,ex.foo2(300,180),0.00);
 	}
+
+	@Test
+	public void aMajorbCosbTrue(){
+		assertEquals(2,ex.foo2(45,0),0.00);
+	}
+
+	@Test
+	public void bMajoraCosaTrue(){
+		assertEquals(2,ex.foo2(180,300),0.00);
+	}
+
+	@Test
+	public void bMajoraCosbTrue(){
+		assertEquals(2,ex.foo2(0,45),0.00);
+	}
+
+	@Test
+	public void aequalsbAllFalse(){
+		assertEquals(90,ex.foo2(0,180),0.00);
+	}
+
 
 }
