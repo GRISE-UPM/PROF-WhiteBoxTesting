d053f3f0293b2a8a8920e9687a64eb6836ce49f7
Jonatan <jonatanruedasmora@gmail.com>
2019-10-01 21:07:17 +0200

    Adds foo1 tests

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -1,14 +1,29 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class Test_foo1 {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLessOrEqualThanFive() {		
+
+		float x = 1.0f, y = 1.0f, expected = y/x;
+		executeTest(x, y, expected);
 	}
 
+	@Test
+	public void testGreaterThanFive() {		
+		
+		float x = 5.1f, y = 1.0f, expected = y/(x+10);
+		executeTest(x, y, expected);
+	}
+	
+	private void executeTest(float x, float y, float expected) {
+		
+		Exercises exercises = new Exercises();
+		float result = exercises.foo1(x, y);
+		assertEquals(result, expected, 0.0f);
+	}
 }
