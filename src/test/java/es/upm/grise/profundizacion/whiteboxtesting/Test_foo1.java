23e853ac5cea7580fdfc6e8125e0363614326172
Eduard Aymerich <eduardaymerich@gmail.com>
2019-10-03 01:03:37 +0200

    test foo1 done

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -1,14 +1,37 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_foo1 {
 
+	Exercises exercise;
+
+	@Before
+	public void setup() {
+		exercise = new Exercises();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void number_x_positive_y_positive_OK() {
+		assertEquals(2.0, exercise.foo1(1, 2), 0.01);
+	}
+
+	@Test
+	public void number_x_negative_y_positive_OK() {
+		assertEquals(-2.0, exercise.foo1(-1, 2), 0.01);
+	}
+
+	@Test
+	public void number_x_positive_y_negative_OK() {
+		assertEquals(-2.0, exercise.foo1(1, -2), 0.01);
+	}
+
+	@Test
+	public void number_x_negative_y_negative_OK() {
+		assertEquals(2.0, exercise.foo1(-1, -2), 0.01);
 	}
 
 }
