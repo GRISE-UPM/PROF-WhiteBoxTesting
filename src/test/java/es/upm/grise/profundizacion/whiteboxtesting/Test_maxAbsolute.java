fe5522a907e0de5a7d8d702eeead1fa01bf1f173
Jonatan <jonatanruedasmora@gmail.com>
2019-10-02 21:24:15 +0200

    Adds max_absolute tests

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -1,14 +1,56 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_maxAbsolute {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private Exercises exercises;
+
+	@Before
+	public void setUp() {
+
+		exercises = new Exercises();
 	}
 
+	@Test
+	public void testPath1() {		
+
+		int[] numbers = { 1, 2, 3, 4, 5, 6};
+		executeTest(numbers, -1);
+	}
+
+	@Test
+	public void testPath2() {		
+
+		int[] numbers = { 1, -20, 3, 4, 5};
+		executeTest(numbers, 20);
+	}
+
+	@Test
+	public void testPath3() {		
+
+		int[] numbers = { 1, 2, 3, 4, 5};
+		executeTest(numbers, 5);
+	}
+
+	@Test
+	public void testPath4() {		
+
+		executeTest(new int[0], 0);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testPath5() {		
+
+		exercises.maxAbsolute(null);
+	}
+
+	private void executeTest(int[] numbers, int expected) {
+
+		int result = exercises.maxAbsolute(numbers);
+		assertEquals(result, expected);
+	}
 }
