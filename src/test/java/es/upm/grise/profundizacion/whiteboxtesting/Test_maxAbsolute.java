260871a2b0f9e0c77ad74e77b9e73aa136d3205e
Eduard Aymerich <eduardaymerich@gmail.com>
2019-10-03 02:24:49 +0200

    test maxAbsolute done

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -1,14 +1,31 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_maxAbsolute {
 
+	Exercises exercise;
+
+	@Before
+	public void setup() {
+		exercise = new Exercises();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void length_numbers_longer_than_5_digits() {
+		int [] numbers = { 9, -9, 9, -9, 9, -9 };
+
+		assertEquals(-1, exercise.maxAbsolute(numbers));
+	}
+
+	@Test
+	public void max_number_OK() {
+		int [] numbers = { 1, -2, 3, -4, 5 };
+
+		assertEquals(5, exercise.maxAbsolute(numbers));
 	}
 
 }
