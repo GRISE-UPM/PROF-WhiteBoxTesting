be2eb54f47f9194782d4053d7c25338a170f3f12
Rafata8 <43552392+Rafata8@users.noreply.github.com>
2019-10-04 02:45:37 +0200

    foo1 y foo2 hechos

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -1,14 +1,21 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class Test_foo1 {
 
+	//el assert equal le he puesto el parametro delta porque si no
+	//dice que esta deprecado
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void test1() {
+		Exercises ejercicio = new Exercises();
+		assertEquals(1, ejercicio.foo1(2, 2),0.001);	}
+	
+	@Test
+	public void test2() {
+		Exercises ejercicio = new Exercises();
+		assertEquals(1, ejercicio.foo1(6, 16),0.001);	}
 
 }
--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
@@ -6,9 +6,51 @@ import org.junit.Test;
 
 public class Test_foo2 {
 
+	//explicado rapido de cuantos caminos hay (lo he dibujado tambien y
+	//hecho la cuenta como en clase:
+	//hay 2 branches en el primero y 4 en el segundo if, por tanto, 
+	//
+	
+	//primer camino, todo es falso
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		Exercises ejercicio = new Exercises();
+		float a=(float) 1.5;
+		float b=(float) 3.1;
+		
+		assertEquals(2.3, ejercicio.foo2(a, b),0.001);	
 	}
+	
+	//segundo camino, el primer if falso y el segundo es falsa 
+	//la primera parte
+	@Test
+	public void test2() {
+		Exercises ejercicio = new Exercises();
+		float a=(float) 1.4;
+		float b=(float) 1.5;
+		
+		assertEquals(2, ejercicio.foo2(a, b),0.001);	
+	}
+	
+	//tercer camino, todo es true ahora (menos la ultima condicion del ultimo if)
+	@Test
+	public void test3() {
+		Exercises ejercicio = new Exercises();
+		float a=(float) 3.14;
+		float b=(float) 3.13;
+		
+		assertEquals(2, ejercicio.foo2(a, b),0.001);	
+	}
+	
+	//me salen solo 3 caminos, no lo entiendo. Pensaba que me saldrian mas, pero
+	//si hago mas repito branches y mis calculos dan que hay solo 
+	//3 caminos sin repetir
+	//con estos 3 caminos tengo toda la cobertura asi que ya esta
+
+	
+
+
+	
+
 
 }
