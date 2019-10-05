5a4028dcdda93def81b2d04daf0b8fb829245282
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-10-05 12:58:57 +0200

    Add maxAbsolute 100% coverage.

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -4,11 +4,53 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Test_maxAbsolute {
+
+	////////////////////////////////////////////////// RANDOMIZATION ///////////////////////////////////////////////////
+
+	private static final Random random = new SecureRandom();
+
+	///////////////////////////////////////////////////// TESTING //////////////////////////////////////////////////////
+
+	private Exercises exercises = new Exercises();
+	
+	@Test
+	public void test_exception() {
+		try {
+			exercises.maxAbsolute(null);
+		} catch (Exception e ) {
+			assertTrue(e instanceof NullPointerException);
+		}
+	}
+	
+	@Test
+	public void test_empty() {
+		int result = exercises.maxAbsolute(new int[0]);
+		assertEquals(0, result);
+	}
+
+	@Test
+	public void test_limit() {
+		int[] numbers = new int[6 + random.nextInt(0xFF - 5)];
+		int result = exercises.maxAbsolute(numbers);
+		assertEquals(-1, result);
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		int[][] numberss = new int[][] {
+				new int[]{ 3, 3, 3, 3, -2 },
+				new int[]{ 1, 1, 1, 1, -2 }
+		};
+		for (int[] numbers : numberss) {
+			int result = exercises.maxAbsolute(numbers);
+			int expected = Arrays.stream(numbers).map(Math::abs).max().orElse(-1);
+			assertEquals(expected, result);
+		}
 	}
 
 }
