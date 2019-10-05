85af95e6af899c7cc02090bed65b0f1babd0704b
Adrian Rodriguez <ardguezsocorro@gmail.com>
2019-10-05 21:32:01 +0200

    Pruebas para maxAbsolute & convertInt

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
@@ -1,14 +1,42 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class Test_convertInt {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	public Exercises testExercise = new Exercises();
+	public Exercises.convertInt varTestConvert = testExercise.new convertInt();
+	public char[] testr;
+	
+	@Test(expected = Exception.class)
+	public void test1() throws Exception {
+		varTestConvert.convert(testr = new char[]{'q','w','e','r','t','y','u'} );
 	}
+	
+	
+	@Test
+	public void test2() throws Exception {
+		assertEquals(1067,varTestConvert.convert(testr = new char[] {'a','a'}));
+	}
+	
+	@Test
+	public void test3() throws Exception{
+		assertEquals(-1022, varTestConvert.convert(testr = new char[] {'-','c',' '}));
+		
+		
+	}
+	
+	@Test(expected = Exception.class)
+	public void test4() throws Exception {
+		varTestConvert.convert(testr = new char[]{'q','w','e','r','t','y'} );	
+	}
+	
+	@Test(expected = Exception.class)
+	public void test5() throws Exception{
+		varTestConvert.convert(testr = new char[] {'-','a','a','a','z'});
+	}
+	
 
 }
