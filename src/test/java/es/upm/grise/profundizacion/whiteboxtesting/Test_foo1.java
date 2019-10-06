fac8ef27f54f5b83ad464e6b70a93f33c7b775ee
Fernando Alonso <1falonso97@gmail.com>
2019-10-06 14:00:50 +0200

    100 % coverage test_foo1

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -2,13 +2,26 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_foo1 {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Exercises exercises;
+	public static final float DELTA =  0.001F;
+
+	@Before
+	public void setUp() {
+		exercises = new Exercises();
 	}
 
+	@Test
+	public void testXLowerThan5() {
+		assertEquals(24 / 4, exercises.foo1(4, 24),DELTA);
+	}
+	
+	@Test
+	public void testXGreaterThan5() {
+		assertEquals(24F / (6+10), exercises.foo1(6, 24),DELTA);
+	}
 }
