05514a600dfe2919915882cf0ae8253717dddcbc
Fernando Alonso <1falonso97@gmail.com>
2019-10-06 18:43:05 +0200

    100 % coverage maxAbsolutes

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -2,13 +2,46 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_maxAbsolute {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Exercises exercises;
+	public static final float DELTA =  0.001F;
+	
+	@Before
+	public void setUp() {
+		exercises = new Exercises();
 	}
-
+	
+	@Test
+	public void testLengthGreaterThan5() {
+		assertEquals(-1, exercises.maxAbsolute(new int []{1,5,7,1,5,7}));
+	}
+	
+	@Test
+	public void testLength0() {
+		assertEquals(0, exercises.maxAbsolute(new int[]{}));
+	}
+	
+	@Test
+	public void testLength1() {
+		assertEquals(1, exercises.maxAbsolute(new int[]{-1}));
+	}
+	
+	@Test
+	public void testNoNegatives() {
+		assertEquals(5, exercises.maxAbsolute(new int []{1,5}));
+	}
+	
+	@Test
+	public void testAllNegatives() {
+		assertEquals(5, exercises.maxAbsolute(new int []{-1,-5}));
+	}
+	
+	@Test
+	public void testSignedAlterned() {
+		assertEquals(8, exercises.maxAbsolute(new int []{-1,5,-3,8,-4}));
+	}
 }
