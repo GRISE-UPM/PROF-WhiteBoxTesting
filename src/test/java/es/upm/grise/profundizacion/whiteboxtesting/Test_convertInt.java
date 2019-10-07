f39b48625f0638aa0c6f683216909e179753bf55
JMonjo <55985859+JMonjo@users.noreply.github.com>
2019-10-08 01:53:46 +0200

    Cambios 4a

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
@@ -2,13 +2,52 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import es.upm.grise.profundizacion.whiteboxtesting.Exercises.convertInt;
 
 public class Test_convertInt {
 
+	private Exercises test;
+	private Exercises.convertInt convertInt;
+	
+	@Before
+	public void before() {
+		test = new Exercises();
+		convertInt = test.new convertInt();
+	}
+	
+	@Test(expected = Exception.class)
+	public void test1_convertInt() throws Exception {
+		char[] str = {'0','1','2','3','4','5','6','7'};
+		convertInt.convert(str);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test2_convertInt() throws Exception {
+		char[] str = {'-','1','2','3'};
+		int exp = -5451;
+		assertEquals(exp, convertInt.convert(str));
+	}
+	
+	@Test
+	public void test3_convertInt() throws Exception {
+		char[] str = {'1','2'};
+		int exp = 540;
+		assertEquals(exp, convertInt.convert(str));
+	}
+	
+	@Test(expected = Exception.class)
+	public void test4_convertInt() throws Exception {
+		char[] str = {'a','b','c','d'};
+		convertInt.convert(str);
+	}
+	
+	@Test(expected = Exception.class)
+	public void test5_convertInt() throws Exception {
+		char[] str = {'-','a','b','c','d'};
+		convertInt.convert(str);	
 	}
 
 }
--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
@@ -2,13 +2,44 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Test_foo2 {
 
+	private Exercises test;
+	
+	@Before
+	public void before() {
+		test = new Exercises();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1_foo2() {
+		float a = 50; float b = 90;
+		float exp = 70;
+		assertEquals(exp, test.foo2(a, b), 0);
+	}
+	
+	@Test
+	public void test2_foo2() {
+		float a = -45; float b = 0;
+		float exp = 2;
+		assertEquals(exp, test.foo2(a, b), 0);
+	}
+	
+	@Test
+	public void test3_foo2() {
+		float a = 0; float b = -45;
+		float exp = 2;
+		assertEquals(exp, test.foo2(a, b), 0);
+	}
+	
+	@Test
+	public void test4_foo2() {
+		float a = -45; float b = -90;
+		float exp = 2;
+		assertEquals(exp, test.foo2(a, b), 0);
 	}
 
 }
