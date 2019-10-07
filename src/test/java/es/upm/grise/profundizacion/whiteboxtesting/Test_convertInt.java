ea9c49309162d8ced72a5df2777f4ed100c41fec
LSDRH <lauras14@ucm.es>
2019-10-07 10:12:59 +0200

    tests for convert()

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
@@ -5,10 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Test_convertInt {
-
+	Exercises ej = new Exercises();
+	Exercises.convertInt convInt = ej.new convertInt();
+	
+	@Test(expected = Exception.class)
+	public void test1() throws Exception{
+		char [] str = new char[] {'\1', '\0', '\0', '\0', '\0', '\0', '\0'};
+		convInt.convert(str);
+	}
+	@Test(expected = Exception.class)
+	public void test2() throws Exception{
+		char [] str = new char[] {'\4', '\0', '\0', '\0', '\0'};
+		convInt.convert(str);
+	}
+	@Test(expected = Exception.class)
+	public void test3() throws Exception{
+		char [] str = new char[] {'-', '\4', '\0', '\0', '\0', '\0'};
+		convInt.convert(str);
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test4() throws Exception {
+		char [] str = new char[] {'\1'};
+		assertEquals(1, convInt.convert(str));
+	}
+	@Test
+	public void test5() throws Exception {
+		char [] str = new char[] {'-', '\1'};
+		assertEquals(-1, convInt.convert(str));
 	}
 
 }
