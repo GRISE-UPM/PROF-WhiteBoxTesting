4c22c162bc02fe04d83fc8ec44ceab5d434fb076
jose-dp <55983469+jose-dp@users.noreply.github.com>
2019-11-08 18:55:02 +0100

    Update Test_foo1.java

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -6,9 +6,15 @@ import org.junit.Test;
 
 public class Test_foo1 {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test // C1: 1-2-3
+	public void test1() {
+		Exercises o = new Exercises();
+		assertEquals(1, o.foo1(6, 16), 0);
 	}
-
+	
+	@Test // C2: 1-3
+	public void test2() {
+		Exercises o = new Exercises();
+		assertEquals(1, o.foo1(5, 5), 0);
+	}
 }
