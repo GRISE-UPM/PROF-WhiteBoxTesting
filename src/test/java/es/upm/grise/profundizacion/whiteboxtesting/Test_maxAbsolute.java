e5cbf989827ac67d26865e139c7c5630b9908f19
jose-dp <55983469+jose-dp@users.noreply.github.com>
2019-11-08 18:55:41 +0100

    Update Test_maxAbsolute.java

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -6,9 +6,32 @@ import org.junit.Test;
 
 public class Test_maxAbsolute {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test // C1: 1-3-4-8-9
+	public void test1() {
+		Exercises o = new Exercises();
+		int [] array = {};
+		assertEquals(0, o.maxAbsolute(array), 0);
+	}
+	
+	@Test // C1: 1-2-9
+	public void test2() {
+		Exercises o = new Exercises();
+		int [] array = {1, 1, 1, 1, 1, 1};
+		assertEquals(-1, o.maxAbsolute(array), 0);
+	}
+	
+	@Test // C1: 1-3-4-5-6-4-8-9
+	public void test3() {
+		Exercises o = new Exercises();
+		int [] array = {-1};
+		assertEquals(1, o.maxAbsolute(array), 0);
+	}
+	
+	@Test // C1: 1-3-4-5-7-4-8-9
+	public void test4() {
+		Exercises o = new Exercises();
+		int [] array = {1};
+		assertEquals(1, o.maxAbsolute(array), 0);
 	}
 
 }
