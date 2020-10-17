26e7090a830bf30685dd0e4fe7f93c31f9728ae4
Sol Dominguez Carnuccio <sol.dominguez.carnuccio@alumnos.upm.es>
2020-10-17 17:36:23 +0200

    implementar tests

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
@@ -6,9 +6,43 @@ import org.junit.Test;
 
 public class Test_foo2 {
 
+	Exercises claseExercises = new Exercises();
+	float pi = (float)Math.PI;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAMayorB() {
+	//a mayor que b: 1-2-3
+		assertEquals(2,claseExercises.foo2(pi/3,0),0.001);
+	}
+	
+	@Test
+	public void testAMenorB() {
+	//a menor que b: 1-3
+		assertEquals(2,claseExercises.foo2(0,pi/3),0.001);
 	}
 
+	@Test
+	public void testCosAMenor0yCosBMayorCero() {
+	//V || V = V: 1-3(-4)-5-6
+		assertEquals(2,claseExercises.foo2(pi,2*pi),0.001); 
+	}	
+	
+	@Test
+	public void testCosAMenor0yCosBMenorCero() {
+	//V || F = V : 1-3(-4)-5-6
+		assertEquals(2,claseExercises.foo2(pi,pi),0.001); 
+	}
+	
+	@Test
+	public void testcosAMayorCeroyCosBMayorCero() {
+	//F || V = V : 1-3-4-5-6
+		assertEquals(2,claseExercises.foo2(0,2*pi),0.001);
+	}	
+
+	@Test
+	public void testcosAMayorCeroyCosBMenorCero() {
+	//F || F = F : 1-3-4-6
+		assertEquals(pi/2,claseExercises.foo2(0,pi),0.001);
+	}		
+		
 }
--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -5,10 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Test_maxAbsolute {
+	
+	Exercises claseExercises = new Exercises();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	//Testeo 1-2
+	public void testVectorMasCincoElementos() {
+		int[] vectorSeisElementos={1,2,3,4,5,6};
+		assertEquals(-1,claseExercises.maxAbsolute(vectorSeisElementos));
+	}
+	
+	@Test
+	//Testeo 1-3-5-6
+	public void testNrosPositivos() {
+		int[] vector={1,2,5,4,3};
+		assertEquals(5,claseExercises.maxAbsolute(vector));
 	}
 
+	@Test
+	//Testeo 1-3-4-5-6
+	public void testNrosNegativos() {
+		int[] vector={1,-2,-5,4,3};
+		assertEquals(5,claseExercises.maxAbsolute(vector));
+	}
+	
 }
