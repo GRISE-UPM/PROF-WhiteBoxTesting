5f86779667dbc186480197656db2b6a8d4da2eb3
Rodrigo Arias <rodriblue8@gmaiil.com>
2020-11-08 17:00:42 +0100

    Tests terminados

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -1,14 +1,27 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class Test_foo1 {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private static Exercises exercise;
+	
+	@BeforeAll
+	public static void before() {
+		exercise = new Exercises();
 	}
 
+	@Test
+	public void foo1_x_mayor_5() {
+		assertEquals(0.5F, exercise.foo1(10F, 10F));
+	}
+	
+	@Test
+	public void foo1_x_menor_5() {
+		assertNotEquals(Float.NaN, exercise.foo1(0F, 0F));
+	}
 }
--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
@@ -1,14 +1,48 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class Test_foo2 {
 
+	private static Exercises exercise;
+
+	@BeforeAll
+	public static void before() {
+		exercise = new Exercises();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void foo2_0_2_a_no_mayor_y_no_entra_en_condicion() {
+		assertEquals(1F, exercise.foo2(0F, 2F));
+	}
+
+	@Test
+	public void foo2_2_2_a_no_mayor_y_si_entra_en_condicion_izquierda() {
+		assertEquals(2F, exercise.foo2(2F, 2F));
+	}
+
+	@Test
+	public void foo2_0_0_a_no_mayor_y_si_entra_en_condicion_derecha() {
+		assertEquals(2F, exercise.foo2(0F, 0F));
+	}
+	
+	@Test
+	public void foo2_2_0_a_mayor_b_y_no_entra_en_condicion() {
+		assertEquals(2F, exercise.foo2(1F, -2F));
+	}
+	
+	
+	@Test
+	public void foo2_2_0_a_mayor_b_y_si_entra_en_condicion_izquierda() {
+		assertEquals(2F, exercise.foo2(2F, -2F));
+	}
+	
+	@Test
+	public void foo2_2_0_a_mayor_b_y_si_entra_en_condicion_derecha() {
+		assertEquals(2F, exercise.foo2(1F, 0F));
 	}
 
 }
