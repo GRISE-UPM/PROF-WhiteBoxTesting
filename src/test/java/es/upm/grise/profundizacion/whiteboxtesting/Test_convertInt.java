9fc6fe6d2453ae113c01f31633ceff98afb36d7a
Rodrigo Arias <rodriblue8@gmaiil.com>
2020-11-08 12:42:07 +0100

    Tests terminados

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
@@ -1,14 +1,56 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import es.upm.grise.profundizacion.whiteboxtesting.Exercises.convertInt;
 
 public class Test_convertInt {
 
+	private static convertInt converter;
+	
+	@BeforeAll
+	public static void before() {
+		converter = new Exercises().new convertInt();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_throws_exception_size6() {
+		Assertions.assertThrows(Exception.class, () -> {
+			converter.convert(new char[] {'a','b','c','d','e','f'});
+		});
+	}
+	
+	@Test
+	public void test_0() throws Exception {
+		assertEquals(0, converter.convert(new char[] {'0','0','0','0','0','0'}));
+	}
+	
+	@Test
+	public void test_negative() throws Exception {
+		assertEquals(-10, converter.convert(new char[] {'-','1','0'}));
+	}
+	
+	@Test
+	public void test_positive() throws Exception {
+		assertEquals(10, converter.convert(new char[] {'1','0'}));
+	}
+	
+	@Test
+	public void test_big_positive() throws Exception {
+		Assertions.assertThrows(Exception.class, () -> {
+			converter.convert(new char[] {'3','2','7','6','8'});
+		});
+	}
+	
+	@Test
+	public void test_small_negative() throws Exception {
+		Assertions.assertThrows(Exception.class, () -> {
+			converter.convert(new char[] {'-','3','2','7','6','9'});
+		});
 	}
 
 }
--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -2,13 +2,36 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class Test_maxAbsolute {
+	
+	private static Exercises exercise;
+
+	@BeforeAll
+	public static void before() {
+		exercise = new Exercises();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_maxAbsolute_array_3() {
+		assertEquals(3,exercise.maxAbsolute(new int [] {1,2,3}));
+	}
+	
+	@Test
+	public void test_maxAbsolute_array_vacio() {
+		assertEquals(0,exercise.maxAbsolute(new int [] {}));
+	}
+	
+	@Test
+	public void test_maxAbsolute_array_negativos() {
+		assertEquals(3,exercise.maxAbsolute(new int [] {-1,-2,-3}));
+	}
+	
+	@Test
+	public void test_maxAbsolute_array_size6() {
+		assertEquals(-1,exercise.maxAbsolute(new int [] {-1,-2,-3,2,3,1}));
 	}
 
 }
