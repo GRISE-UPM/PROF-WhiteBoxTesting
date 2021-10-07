0f7942764e14968d4c838a902559bfc0a4c03349
Charly98cma <1998cma@gmail.com>
2021-10-08 00:28:13 +0200

    Completed maxAbsolute tests
    Added some imports to improve code and results readability, and added multiple tests of maxAbsolute with some limit cases

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -3,12 +3,50 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
 
 public class Test_maxAbsolute {
 
+	private static Exercises exercises;
+
+	@BeforeAll
+	void setUp() {
+		System.out.println("Setting up foo2 test...");
+		exercises = new Exercises();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@DisplayName("Test with an array of more that 5 elements that should return -1")
+	void arrayOfLessThanFiveElements() {
+		assertEquals(-1, exercises.maxAbsolute(new int[6]), 0);
+	}
+
+	@Test
+	@DisplayName("Test with an array of zeroes that should return 0")
+	void maxOfArrayWithAllZeroes() {
+		int[] numbers = {0, 0, 0, 0};
+		assertEquals(0, exercises.maxAbsolute(numbers), 0);
+	}
+
+	@Test
+	@DisplayName("Test with an array of negative numbers that should return 3")
+	void maxOfArrayWithAllNegativeIntegers() {
+		int[] numbers = {-2, -1, -3};
+		assertEquals(3, exercises.maxAbsolute(numbers), 0);
+	}
+
+	@Test
+	@DisplayName("Test with an array of positive and negative numbers that should return 4")
+	void maxOfArrayWithNegAndPosNumbers() {
+		int[] numbers = {-3, -1, 4};
+		assertEquals(3, exercises.maxAbsolute(numbers), 0);
+	}
+
+	@AfterAll
+	void tearDown() {
+		System.out.println("foo2 tests done.");
 	}
 
 }
