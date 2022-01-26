ca910a8fa5f806278669664e5d1ded91afa6073d
EnricPP <enricplanas2@gmail.com>
2022-01-26 12:50:28 +0100

    .

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1.java
@@ -1,45 +1,32 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Test_foo1 {
 
-	@BeforeAll
-	public void beforeAll() {
-		System.out.println("--foo1 Tests--");
-	}
+	private Exercises ex = new Exercises();
 
 	@BeforeEach
 	public void beforeEach() {
-		System.out.println("Nuevo test:");
+		System.out.println("Nuevo test_foo1:");
 	}
 
 	@Test
-	@DisplayName("x>5 returns y/(x + 10)")
-	public void xBigger5() {
-		Exercises exercises = new Exercises();
-		float x = 6;
-		float y = 3;
-		float expected = y/(x+10);
-
-		assertEquals(expected, exercises.foo1(x,y), 0);
-;	}
+	@DisplayName("x>5")
+	public void foo1_test1() {
+		assertEquals(0.5, ex.foo1(10,10), 0);
+	}
 
 	@Test
-	@DisplayName("x<5 returns y/x")
-	public void xSmaller5() {
-		Exercises exercises = new Exercises();
-		float x = 4;
-		float y = 3;
-		float expected = y/x;
-
-		assertEquals(expected, exercises.foo1(x,y), 0);
+	@DisplayName("x<=5")
+	public void foo1_test2() {
+		assertEquals(2, ex.foo1(5,10), 0);
 	}
+
 
 }
--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
@@ -1,37 +1,40 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Test_foo2 {
 
-	@BeforeAll
-	public void beforeAll() {
-		System.out.println("--foo2 Tests--");
-	}
+	private Exercises ex = new Exercises();
 
 	@BeforeEach
 	public void beforeEach() {
-		System.out.println("Nuevo test:");
+		System.out.println("Nuevo test_foo2:");
+	}
+
+
+	@Test
+	@DisplayName("a>b")
+	public void foo2_test1() {
+		assertEquals(7.5, ex.foo2(10,5), 0);
 	}
 
 	@Test
-	@DisplayName("a>b returns 2")
-	public void aBiggerb() {
+	@DisplayName("a<b")
+	public void foo2_test2() {
 		Exercises exercises = new Exercises();
-		int a = 5;
-		int b = 3;
+		int a = 3;
+		int b = 10;
 
 		assertEquals(2, exercises.foo2(a, b), 0);
 	}
 
 	@Test
-	@DisplayName("a<b returns 2")
-	public void aSmallerb() {
+	@DisplayName("a<b, segundo if no se cumple")
+	public void foo2_test3() {
 		Exercises exercises = new Exercises();
 		int a = 3;
 		int b = 10;
