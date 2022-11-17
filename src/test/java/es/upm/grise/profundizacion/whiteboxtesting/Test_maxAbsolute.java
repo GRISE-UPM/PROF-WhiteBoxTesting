6e8c5110d4dfdb3d2cbab37d46d9343a1d5da43a
dlgcjorge <dlgcjorge@outlook.es>
2022-11-17 20:17:32 +0100

    test(maxAbsolute): implementación de los tests para maxAbsolute.

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -1,14 +1,47 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class Test_maxAbsolute {
 
+private final Exercises exercises = new Exercises();
+	
+	@BeforeAll
+	static public void beforeAll() {
+		System.out.println("Tests of foo2 has started.");
+	}
+	
+	@AfterAll
+	static public void afterAll() {
+		System.out.println("Tests of foo2 has finished.");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmpty() {
+		int [] numbers = new int[] {};
+		assertEquals(0, exercises.maxAbsolute(numbers));
+	}
+	
+	@Test
+	public void testLengthGT5() {
+		int [] numbers = new int[] {0,1,2,3,4,5};
+		assertEquals(-1, exercises.maxAbsolute(numbers));
+	}
+	
+	@Test
+	public void testLengthLE5() {
+		int [] numbers = new int[] {0,1,2,3,4};
+		assertEquals(4, exercises.maxAbsolute(numbers));
+	}
+	
+	@Test
+	public void testLengthLE5NegAndPos() {
+		int [] numbers = new int[] {-2,-1,0,1};
+		assertEquals(2, exercises.maxAbsolute(numbers));
 	}
 
 }
