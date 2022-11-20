f315ef37486aee4fa7742e65c1e1b69f176cbbbf
juan <juan@MSI.home>
2022-11-20 14:31:56 +0100

    primer test

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import es.upm.grise.profundizacion.whiteboxtesting.Exercises.convertInt;
+
 public class Test_convertInt {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws Exception {
+		Exercises exc = new Exercises();
+		convertInt converter = exc.new convertInt();
+		
+		char[] num = {'9','\0'};
+		
+		assertEquals(9,converter.convert(num));
+		//fail("Not yet implemented");
 	}
 
 }
