1ad22fbc6678f539311974d1b6ada08647617058
juan <juan@MSI.home>
2022-11-20 14:42:18 +0100

    test code

---
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertIntTestTest.java
@@ -0,0 +1,23 @@
+package es.upm.grise.profundizacion.whiteboxtesting;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import es.upm.grise.profundizacion.whiteboxtesting.Exercises.convertInt;
+
+class Test_convertIntTestTest {
+
+	@Test
+	void test() throws Exception {
+		Exercises exc = new Exercises();
+		convertInt converter = exc.new convertInt();
+		
+		char[] num = {'9','\0'};
+		
+		assertEquals(9,converter.convert(num));
+		// fail("Not yet implemented");
+	}
+
+}
