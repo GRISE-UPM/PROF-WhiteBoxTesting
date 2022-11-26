28ed27309776fac98c83c2176d8d524c5c3460cf
mariapizarro1 <maria.pizarro.gomez@alumnos.upm.es>
2022-11-26 18:50:26 +0100

    junit test Maria Pizarro

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2.java
@@ -1,14 +1,41 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+
 
 public class Test_foo2 {
 
+	
+	//a = 5, b = 7.
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_foo2() {
+		Exercises ejercicio = new Exercises();
+		float a = 5;
+		float b = 7;
+		assertEquals(2.0, ejercicio.foo2(a, b), 0.001);
+			
 	}
-
+	
+	//a = 7, b = 6.
+	@Test
+	public void test_foo2_2() {
+		Exercises ejercicio = new Exercises();
+		float a = 5;
+		float b = 7;
+		assertEquals(2.0, ejercicio.foo2(a, b), 0.001);
+			
+	}
+	
+	//a = 12, b = 6.
+	@Test
+	public void test_foo2_3() {
+		Exercises ejercicio = new Exercises();
+		float a = 5;
+		float b = 7;
+		assertEquals(2.0, ejercicio.foo2(a, b), 0.001);
+			
+	}
 }
