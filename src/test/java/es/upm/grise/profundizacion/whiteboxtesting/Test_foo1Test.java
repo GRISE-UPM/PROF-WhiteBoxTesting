8994a5edc550cfa7325846c1a625ef84c7053ced
Juan <49064426+JuanPMC@users.noreply.github.com>
2023-01-01 12:43:48 +0100

    Update Test_foo1Test.java

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1Test.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo1Test.java
@@ -1,16 +1,26 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.jupiter.api.Test;
 
-class Test_foo1Test {
 
-	@SuppressWarnings("deprecation")
+class Test_foo1Test {
 	@Test
 	void test() {
 		Exercises exc = new Exercises();
-
-		assertEquals(2,exc.foo1(6,32));
+		float real = exc.foo1(6,32);
+		float ideal = 2;
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+		
+		ideal = 1;
+		real = exc.foo1(1,1);
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+		
 	}
 
 }
