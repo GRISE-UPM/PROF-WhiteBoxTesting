bf1b7853cfb458cee89e43a286db203b12fbdc2f
Juan <49064426+JuanPMC@users.noreply.github.com>
2023-01-01 12:46:03 +0100

    Update Test_maxAbsolute.java

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_maxAbsolute.java
@@ -2,13 +2,36 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class Test_maxAbsolute {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Exercises exc = new Exercises();
+		
+		int[] testeo = {1,4,2,5,-10};
+		
+		float real = exc.maxAbsolute(testeo);
+		int ideal = 10;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+	}
+	
+	@Test
+	public void test_tamanio() {
+		Exercises exc = new Exercises();
+		
+		int[] testeo = {1,4,2,5,-10,11};
+		
+		float real = exc.maxAbsolute(testeo);
+		int ideal = -1;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
 	}
 
 }
