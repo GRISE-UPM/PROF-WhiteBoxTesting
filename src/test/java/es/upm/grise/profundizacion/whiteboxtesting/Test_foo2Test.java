d3a3d7651921cf7bcf16296110ca0fce4fd962ea
Juan <49064426+JuanPMC@users.noreply.github.com>
2023-01-01 12:44:50 +0100

    Update Test_foo2Test.java

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2Test.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_foo2Test.java
@@ -2,13 +2,49 @@ package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class Test_foo2 {
+public class Test_foo2Test {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Exercises exc = new Exercises();
+		float real = exc.foo2(1,(float)0.5);
+		float ideal = (float) 2.0;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+		
+		real = exc.foo2(1,(float)2);
+		ideal = (float) 1.5;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+		
+		real = exc.foo2((float) 1,(float)1);
+		ideal = (float) 2;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+		
+		real = exc.foo2((float) 2,(float)1);
+		ideal = (float) 1.5;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
+		
+		real = exc.foo2((float) 2,(float)2);
+		ideal = (float) 2;
+		
+		if(real != ideal) {
+			fail("Ideal distinto del real");
+		}
 	}
+	
+	
 
 }
