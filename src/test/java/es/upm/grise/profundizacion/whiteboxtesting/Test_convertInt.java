6dc13397c021160a751bfd5c551c335b5e44a91c
mdelucasg <delucasgarcia.mario@gmail.com>
2023-01-27 19:36:56 +0100

    Add structural test for converInt

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertInt.java
@@ -28,4 +28,27 @@ public class Test_convertInt {
 		assertEquals(-1234, convertInt.convert(str), 0.00001);
 	}
 
+	// Structural test case - 6 areas but overlapping
+	// 1. str.length > 6
+	@Test
+	public void strBigger6Test2() {
+		char[] str = {'1', '2', '3', '4', '5', '6', '7'};
+		assertThrows(Exception.class, () -> convertInt.convert(str));
+	}
+	// 2. str.length < 6 && str[0] != '-' && number > 32767
+	@Test
+	public void strLess6WithHyphenBigger32767Test() {
+		char[] str = {'4', '2', '7', '6', '8'};
+		assertThrows(Exception.class, () -> convertInt.convert(str));
+	}
+	// 3. str.length < 6 && str[0] == '-' && number < -32767
+	@Test
+	public void strLess6WithHyphenLess32767Test() throws Exception {
+		char[] str = {'-', '4', '2', '7', '6', '8'};
+		assertEquals(-32766, convertInt.convert(str));
+	}
+
+	
+
+
 }
